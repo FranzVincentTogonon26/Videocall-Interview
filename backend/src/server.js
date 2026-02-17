84a5e439735d5667be401b449777357180f796ff
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-17 19:57:24 +0800

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
