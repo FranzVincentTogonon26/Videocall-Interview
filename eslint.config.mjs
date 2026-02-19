6374d778ec9eca6f14ee57263489e557961324e9
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-19 12:20:27 +0800

    Configuring ESLint Setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+	{ files: ["backend/**/*.js"], languageOptions: { globals: globals.node } },
+	{ 
+    files: ["backend/**/*.js"], 
+    plugins: { js, react }, 
+    extends: ["js/recommended"],
+    settings: {
+          react: {
+            version: "detect",
+          },
+        }, 
+  },
 ]);
