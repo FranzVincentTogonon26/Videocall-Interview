0bd7334ec4e054a689d123391dcfca334361d38c
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-20 00:13:14 +0800

    Updateded

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import serve from 'inngest/express'
+import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
+import { functions, inngest } from './config/inngest.js';
 import connectDB from './config/db.js';
 
 dotenv.config({ quiet: true });
@@ -8,9 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(clerkMiddleware());
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Routes
+app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/protected', requireAuth(), async ( req, res ) => {
+   const { userId } = getAuth(req);
+})
 
 connectDB().then(() => {
    app.listen(PORT, () => {
