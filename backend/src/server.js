21263d9476d4302a5d644c5f3a0cc4b0efdd6ad7
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-20 00:21:12 +0800

    Review messey code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import serve from 'inngest/express'
-import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
+import serve from 'inngest/express';
+import { clerkMiddleware } from '@clerk/express';
 import { functions, inngest } from './config/inngest.js';
 import connectDB from './config/db.js';
 
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Routes
 app.use('/api/inngest', serve({ client: inngest, functions }));
-app.use('/protected', requireAuth(), async ( req, res ) => {
-   const { userId } = getAuth(req);
-})
+// app.use('/protected', requireAuth(), async ( req, res ) => {
+//    const { userId } = getAuth(req);
+// })
 
 connectDB().then(() => {
    app.listen(PORT, () => {
